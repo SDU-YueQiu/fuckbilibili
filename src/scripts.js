1813ef3d79c532dee6a4e1bdfe85f74bbfc81a63
The beauty of the wild <iop18653346184@hotmail.com>
2022-10-19 21:33:44 +0800

    我是傻逼

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,14 @@
-function change(now)
-{
-    if(now==1)
-    {
-        document.getElementById("switch").setAttribute("src","../source/img/SVGs/tanmuClose.svg");
-        document.getElementById("swtdiv").setAttribute("onclick","change(0)");
+function change(now) {
+    if (now == 1) {
+        document.getElementById("switch").setAttribute("src", "../source/img/SVGs/tanmuClose.svg");
+        document.getElementById("swtdiv").setAttribute("onclick", "change(0)");
     }
-    else
-    {
-        document.getElementById("switch").setAttribute("src","../source/img/SVGs/tanmuOpen.svg");
-        document.getElementById("swtdiv").setAttribute("onclick","change(1)");
+    else {
+        document.getElementById("switch").setAttribute("src", "../source/img/SVGs/tanmuOpen.svg");
+        document.getElementById("swtdiv").setAttribute("onclick", "change(1)");
     }
+}
+
+function playvideo(videos) {
+    
 }
