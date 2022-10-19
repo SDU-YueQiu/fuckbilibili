ecb3a29d144c3b82dc6e1cd95dfd6fc53569a1d3
The beauty of the wild <iop18653346184@hotmail.com>
2022-10-19 20:17:56 +0800

    bug我给你跪下了，html求求你听话按照我想的结构出现吧T.T

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,13 @@
-function fky()
+function change(now)
 {
-    document.getElementById("fk").innerHTML="fky";
+    if(now==1)
+    {
+        document.getElementById("switch").setAttribute("src","../source/img/SVGs/tanmuClose.svg");
+        document.getElementById("swtdiv").setAttribute("onclick","change(0)");
+    }
+    else
+    {
+        document.getElementById("switch").setAttribute("src","../source/img/SVGs/tanmuOpen.svg");
+        document.getElementById("swtdiv").setAttribute("onclick","change(1)");
+    }
 }
