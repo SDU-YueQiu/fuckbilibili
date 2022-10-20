dbc9b6eff89fa4c53ecc4bfc5c282959fe7115a1
The beauty of the wild <iop18653346184@hotmail.com>
2022-10-20 23:00:21 +0800

    小修小改

--- src/scripts.js
+++ src/scripts.js
@@ -9,6 +9,7 @@ function change(now) {
     }
 }
 function changevideo(name) {
+    document.getElementById("myplayer").setAttribute("onclick","pauseorplay(1)");
     switch (name) {
         default:
             alert("你是怎么触发这个弹窗的¿");
@@ -19,6 +20,14 @@ function changevideo(name) {
     }
 }
 
-function mprint() {
-    alert(myvideo.duration);
+function pauseorplay(now) {
+    if(now==0){
+        alert("选择一个视频再点播放器喵~")
+    }
+    else{
+        if(myvideo.paused==false){
+            myvideo.pause();
+        }
+        else myvideo.play();
+    }
 }
