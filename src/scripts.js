b111888f19eabd80dde6634abe650084181b39c8
The beauty of the wild <iop18653346184@hotmail.com>
2022-10-20 17:00:15 +0800

    于皇是天ORZ

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,3 @@
-var ovideo = document.querySelector("player");
 function change(now) {
     if (now == 1) {
         document.getElementById("switch").setAttribute("src", "../source/img/SVGs/tanmuClose.svg");
@@ -8,10 +7,4 @@ function change(now) {
         document.getElementById("switch").setAttribute("src", "../source/img/SVGs/tanmuOpen.svg");
         document.getElementById("swtdiv").setAttribute("onclick", "change(1)");
     }
-}
-function start() {
-    /* alert("fuckyou"); */
-    document.querySelector("player").setAttribute("src","../source/video/i got smoke.mp4");
-    
-    alert("fuckyou");
 }
