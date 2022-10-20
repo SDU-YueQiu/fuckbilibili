2491f8961c13a2d069f4777842d987b0bc52bb68
The beauty of the wild <iop18653346184@hotmail.com>
2022-10-20 19:22:21 +0800

    爷要开始攻坚破难辣

--- src/scripts.js
+++ src/scripts.js
@@ -8,3 +8,17 @@ function change(now) {
         document.getElementById("swtdiv").setAttribute("onclick", "change(1)");
     }
 }
+function changevideo(name) {
+    switch (name) {
+        default:
+            alert("你是怎么触发这个弹窗的¿");
+            break;
+        case 'igt':
+            myvideo.src = "../source/video/igt.mp4";
+            break;
+    }
+}
+
+function mprint() {
+    alert(myvideo.duration);
+}
