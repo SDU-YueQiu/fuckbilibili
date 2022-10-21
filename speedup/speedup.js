d98524b64d38ddc5a248504f99a69bd416f68764
The beauty of the wild <iop18653346184@hotmail.com>
2022-10-21 16:14:15 +0800

    你所热爱的就是你的生活，我热爱艹死b站他妈

--- speedup/speedup.js
+++ speedup/speedup.js
@@ -1,4 +1,4 @@
-var input=prompt('输入数字改变倍速，输入n切换至夜间模式');
+var input = prompt('输入数字改变倍速，输入n切换至夜间模式');
 console.dir(input);
 if (input == 'n') {
     document.querySelector(".video-container-v1").style.backgroundColor = "#3d3d3d";
@@ -8,8 +8,7 @@ if (input == 'n') {
     document.querySelector(".reply-tag-item").style.backgroundColor = "#3d3d3d";
     document.querySelector("#nav-searchform").style.backgroundColor = "#3d3d3d";
 }
-else{
-    var temp = document.createElement('script');
-    temp.setAttribute('type','text/javascript');
-    temp.innerHTML="document.querySelector('bwp-video').playbackRate = 3;";
+else {
+    var fkvideo = document.querySelector('video');
+    fkvideo.playbackRate=input;
 }
